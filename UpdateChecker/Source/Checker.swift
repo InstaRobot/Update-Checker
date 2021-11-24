2e118ae7c38ba6657d5ec19764db2e42b80b2955
Vitaliy Podolskiy <instarobot.net@gmail.com>
2021-11-25 01:13:16 +0300

    Update Checker.swift

--- UpdateChecker/Source/Checker.swift
+++ UpdateChecker/Source/Checker.swift
@@ -20,8 +20,17 @@ public struct Checker {
     public func start() {
         assert(!configuration.bundleIdentifier.isEmpty)
         
-        if let date = UserDefaults.standard.object(forKey: k_CheckDateInited) as? Date {
-            print(date)
+        if var date = UserDefaults.standard.object(forKey: k_CheckDateInited) as? Date {
+            let calendar = Calendar.current
+            date = calendar.date(byAdding: .day, value: configuration.daysBeforeCheck, to: date) ?? date
+            let result = calendar.compare(date, to: Date(), toGranularity: .day)
+            switch result {
+            case .orderedAscending, .orderedSame:
+                print("can check")
+            default:
+                print("cannot check")
+                return
+            }
         }
         else {
             UserDefaults.standard.set(
@@ -31,7 +40,5 @@ public struct Checker {
             UserDefaults.standard.synchronize()
             start()
         }
-        
-        print(#function)
     }
 }
